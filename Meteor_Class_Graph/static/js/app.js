3ce4afc8fd797606299c10da2c44eab44e2117d1
diamondwyrm <kent.j.hazen@biola.edu>
2019-06-28 15:45:26 -0700

    graph finished

--- Meteor_Class_Graph/static/js/app.js
+++ Meteor_Class_Graph/static/js/app.js
@@ -2,14 +2,14 @@
 dataLocation = "static/data/Clean_Meteor_Class_Data.csv"
 
 margin = {
-	left: 60,
+	left: 100,
 	right: 60,
-	top: 10,
+	top: 60,
 	bottom: 150
 }
 
 svgWidth = 600;
-svgHeight = 375; // *** edit to be adjustable to screen size
+svgHeight = 500; // *** edit to be adjustable to screen size
 
 chartWidth = svgWidth - margin.left - margin.right;
 chartHeight = svgHeight - margin.top - margin.bottom;
@@ -33,19 +33,19 @@ function graph(error) {
 			d.count_of_class = +d.count_of_class;
 		});
 
-		// get the height and width of the graph
+		// scale the height and width of the graph
 		var scaleHeight = d3.scaleSqrt()
 			.domain([0, d3.max(data, function (d) {
 				console.log(d.count_of_class);
 				return d.count_of_class; // ******* this is going to need to be changed to fit wit the item info below
 			})])
-			.range([0, chartHeight]);
+			.range([chartHeight, 0]);
 
 		/* #region  count the number of items in the selected tier */
 		var items = [];
 
 		data.forEach(d => {
-			items.push(d.class_tier_3); // ****** this is going to need to be changed to accecpt variable class tiers
+			items.push(d.class_tier_3);
 		});
 		items = items.sort();
 		
@@ -69,6 +69,8 @@ function graph(error) {
 			.domain([0,subValues.length])
 			.range([0, chartWidth]);
 
+
+		// create Axis
 		var xAxis = d3.axisBottom(scaleWidth)
 			.ticks(subValues.length)
 			.tickFormat(function(d) {
@@ -78,6 +80,7 @@ function graph(error) {
 		var yAxis = d3.axisLeft(scaleHeight)
 			.tickSize(chartWidth);
 
+		// draw axes
 		chartGroup.append("g")
 			.attr("transform", `translate(0, ${chartHeight})`)
 			.call(xAxis)
@@ -93,17 +96,65 @@ function graph(error) {
 		barSpacing = chartWidth / subValues.length * .05;
 
 		barWidth = (chartWidth - (barSpacing * (subValues.length - 1))) / subValues.length;
+		
+		//get the primary classes of meteorites
+		var items2 = [];
 
+		data.forEach(d => {
+			items2.push(d.class_tier_1);
+		});
+		items2 = items2.sort();
+		
+		console.log(items2);
+
+		current = null;
+		primaryClasses = [];
+
+		items2.forEach(item => {
+			if (item != current) {
+				current = item;
+				primaryClasses.push(item);
+			}
+		});
+		console.log(primaryClasses);
+
+		//create colors for bars
+		var myColors = d3.scaleOrdinal()
+			.domain(primaryClasses)
+			.range(["red", "blue", "purple"]);
+
+		//draw graph
 		barGroup = chartGroup.selectAll(".bar")
 			.data(data)
 			.enter()
 			.append("rect")
+			.attr("fill", function(d) {return myColors(d.class_tier_1)})
 			.classed("bar", true)
 			.attr("width", d => barWidth)
- 		   	.attr("height", d => scaleHeight(d.count_of_class))
+ 		   	.attr("height", d => chartHeight - scaleHeight(d.count_of_class))
    			.attr("x", (d, i) => i * (barWidth + barSpacing))
-			.attr("y", d => chartHeight - scaleHeight(d.count_of_class));
+			.attr("y", d => scaleHeight(d.count_of_class));
 
+
+		// add lables
+		chartGroup.append("text")
+			.attr("transform", "rotate(-90)")
+			.attr("y", 0 - margin.left/2)
+			.attr("x", 0 - (chartWidth/2))
+			.style("text-anchor", "middle")
+			.text("Landings per Class");
+
+		chartGroup.append("text")
+			.attr("y",chartHeight + margin.bottom * .99)
+			.attr("x",(chartWidth/2))
+			.style("text-anchor", "middle")
+			.text("Classifications");
+
+		chartGroup.append("text")
+			.attr("y", 0 - margin.top/2)
+			.attr("x",(chartWidth/2))
+			.style("text-anchor", "middle")
+			.text("Meteorite Classifications");
 	});
 
 }
